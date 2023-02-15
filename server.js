aef1e86cc5b2a4307759dca6094a86abcda60337
Sujeet Mainali <mainalisujeet@gmail.com>
2023-02-15 18:19:40 +0545

    added styling added socket for multiple conection

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { v4 : uuidv4 } = require('uuid');
 const app = express();
 const server = require('http').Server(app);
+const io = require('socket.io')(server)
 
 
 
@@ -17,4 +18,12 @@ app.get('/:room', (req, resp)=>{
     resp.render('room',{roomId: req.params.room})
 })
 
-app.listen(3000);
+io.on('connection',socket  =>{
+    socket.on('join-room', (roomId)=>{
+        socket.join(roomId);
+        socket.to(roomId).emit('user-connected');
+        // console.log('joined room');
+    })
+})
+
+server.listen(3000);
